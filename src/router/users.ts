1ccf2aef4d8460849977d0ea5824b774691dbe50
Lucas-Masaba <masabaluke@gmail.com>
2023-10-23 17:32:38 +0300

    Enable delete user

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { isAuthenticated } from '../middlewares';
 
-import { getAllUsers } from '../controllers/users';
+import { deleteUser, getAllUsers } from '../controllers/users';
 
 export default (router: express.Router) => {
   router.get('/users', isAuthenticated, getAllUsers);
+  router.delete('/users/:id', deleteUser);
 }
